3dbb4df397d4f6eab07b02e70dee92bb24978cff
Kenny Truong <ktruong12033@gmail.com>
2013-09-18 18:30:45 -0400

    Initiate game stage

--- web/js/page-game.js
+++ web/js/page-game.js
@@ -1,5 +1,38 @@
 App.populator('page-game', function(page){
-	
-	//TODO
+	var p = $(page);
+	var canvasSection = p.find('#canvas-section');
+	var actionBarItem = p.find('.actions');
+	var optionButton = p.find('#options');
 
+
+	p.on('appShow', function () {
+    	
+    	var CONST = {
+			GAME_HEIGHT : canvasSection.height(),
+			GAME_WIDTH	: canvasSection.width(),
+			GAME_BACKGROUND_COLOUR	: '#222'
+		};
+
+		Game = {
+			start: function(){
+				// Disable crafty.mobile so that the stage elem can have the fixed size
+				Crafty.mobile = false;
+
+				// Initialize craftyjs and set the main stage to the div #stage_elem found in the HTML
+				Crafty.init(CONST.GAME_WIDTH, CONST.GAME_HEIGHT, "stage_elem");
+				Crafty.background(CONST.GAME_BACKGROUND_COLOUR);
+
+				// Draw a blob image at the center of the stage
+				Crafty.e("2D, DOM, Image")
+					.image("../img/blob/blob_normal.png")
+					.attr({
+						x: (CONST.GAME_WIDTH/2) - 32,
+						y: (CONST.GAME_HEIGHT/2) - 32
+					});
+			}
+		};
+
+		Game.start();
+
+  	});
 });
