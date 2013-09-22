c0de179debc3bc2b27cd428e35fc4d1b7015273d
xktse <ktruong12033@gmail.com>
2013-09-21 23:12:41 -0400

    blobsql merge conflicts

--- web/postgresql/blob.sql
+++ web/postgresql/blob.sql
@@ -1,18 +1,5 @@
-INSERT INTO petConfigs VALUES(0, 'Blob', 0, 1, 1, 1, 1);
-<<<<<<< HEAD
-<<<<<<< HEAD
-INSERT INTO petConfigs VALUES(0, 'Blob', 0, 1, 1, 1, 1);
-INSERT INTO petConfigs VALUES(0, 'Blob', 0, 1, 1, 1, 1);
-INSERT INTO petConfigs VALUES(0, 'Blob', 0, 1, 1, 1, 1);
-=======
 INSERT INTO petConfigs VALUES(0, 'Blob', 1, 1, 1, 1, 1);
 INSERT INTO petConfigs VALUES(0, 'Blob', 2, 1, 1, 1, 1);
 INSERT INTO petConfigs VALUES(0, 'Blob', 3, 1, 1, 1, 1);
->>>>>>> 906709e8e43a389ed1289bdaf08eca0e5b7e1e68
-=======
-INSERT INTO petConfigs VALUES(0, 'Blob', 1, 1, 1, 1, 1);
-INSERT INTO petConfigs VALUES(0, 'Blob', 2, 1, 1, 1, 1);
-INSERT INTO petConfigs VALUES(0, 'Blob', 3, 1, 1, 1, 1);
->>>>>>> 906709e8e43a389ed1289bdaf08eca0e5b7e1e68
 INSERT INTO food (petType, foodName, hungerDrop)VALUES('{0}', 'Cookie', 30);
 INSERT INTO activities (petType, activityName, entertainmentIncrease)VALUES('{0}', 'Play Ball', 30);
